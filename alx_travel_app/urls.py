32c5b4d371345d248a1cf0f754ac3662b430bd4c
Israel Duku <israelduku9@gmail.com>
2025-05-18 14:03:00 +1200

    Update urls.py

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -23,3 +23,10 @@ urlpatterns = [
 ]
 
 
+# listings/urls.py
+from django.urls import path
+from .views import HelloWorldView
+
+urlpatterns = [
+    path('hello/', HelloWorldView.as_view(), name='hello-world'),
+]
